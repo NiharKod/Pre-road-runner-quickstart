b3434a0b408539395c18afc2766942c849df9c3d
Nihar Kodkani <38077916+NiharKod@users.noreply.github.com>
2020-07-29 16:36:24 -0400

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/Subsystems/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/Subsystems/Robot.java
@@ -1,11 +1,21 @@
 package org.firstinspires.ftc.teamcode.Drive.Subsystems;
 
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Drive.Libs.StickyGamepad;
 
 public class Robot {
     HardwareMap hw;
    public  SimpleDrivetrain drivetrain;
+
+
+
     public Robot(HardwareMap hardwareMap){
+        this.hw = hardwareMap;
+
+    }
+    public Robot(HardwareMap hardwareMap, Gamepad gamepad){
         this.hw = hardwareMap;
 
     }
